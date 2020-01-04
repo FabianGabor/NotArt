08290bf5284e6ca6ec01bff87dcb7e29d8d935b9
Fábián Gábor <info@fabiangabor.com>
2020-01-04 17:25:28 +0100

    walk spiral

--- num.py
+++ num.py
@@ -5,66 +5,78 @@
 from PIL import Image
 
 # Open our image
-im = Image.open("not_art_crop.png")
+im = Image.open("not_art.png")
+ 
+# Convert our image to RGB
+rgb_im = im.convert('RGB')
 
-# Convert our image to RGB
-rgb_im = im.convert('RGB')
- 
-# Open our image
-im = Image.open("not_art_crop.png")
- 
-# Convert our image to RGB
-rgb_im = im.convert('RGB')
+def rgb2hex(r, g, b):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
 # Use the .size object to retrieve a tuple contain (width,height) of the image
 # and assign them to width and height variables
 width = rgb_im.size[0]
 height = rgb_im.size[1]
+size = rgb_im.size[0]
 
 # set some counters for current row and column and total pixels
-row = 1
-col = 1
-pix = 0
+row = 0
+col = 0
 codel = 10
+codel_count = 0
+color_format = 'hex' # rgb / hex
 
-# create an empty output row
-rowdata = ""
-codels = ""
- 
-# create an empty output row
-rowdata = ""
-codels = ""
+# Get color from the middle of the codel:
+# col + codel / 2, row + codel / 2
 
-# loop through each pixel in each row outputting RGB value as we go...
-# all the plus and minus ones are to deal with the .getpixel class being
-# zero indexed and we want the output to start at pixel 1,1 not 0,0!
-while row < height + 1:
-	while col < width + 1:
+limit = 0
+while limit < size / 2:
+	print("limit = ", limit)
+	while col + codel < size - limit:
+		print("%3d,%3d:" %(row,col))
+		#get the RGB values from the current pixel
+		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
+		if color_format == 'rgb':
+			print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
+		else:
+			print("\t", rgb2hex(r, g, b))
+		col += codel
+		codel_count += 1
+	while row + codel < size - limit:
+		print("%3d,%3d:" %(row,col))
 		# get the RGB values from the current pixel
-		r, g, b = rgb_im.getpixel((col - 1, row - 1))
-		# append the RGB values to the rowdata variable as (R, G, B)
-		rowdata +=  str(r)+str(g)+str(b)
-		codels += str(r) + " " + str(g) + " " + str(b)
-		#print("Col: %2d" %((col - 1) / 10 + 1))
-		print("%3s %3s %3s" %(str(r), str(g), str(b)) )
-		# increment the column count int('052', 8)
-		col = col + codel
-		# increment the pixel count
-		pix = pix + 1		
-		codels = ""
-	# print out all RGB values for the row
-	#print(rowdata)
-	# clear out rowdata variable
-	rowdata = ""
-	# increment the row...
-	row = row + codel
-	# reset the column count
-	col = 1
+		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
+		if color_format == 'rgb':
+			print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
+		else:
+			print("\t", rgb2hex(r, g, b))
+		row += codel
+		codel_count += 1
+	while col - codel >= limit:
+		print("%3d,%3d:" %(row,col))
+		# get the RGB values from the current pixel
+		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
+		if color_format == 'rgb':
+			print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
+		else:
+			print("\t", rgb2hex(r, g, b))
+		col -= codel
+		codel_count += 1
+	limit += codel * 2
+	while row - codel >= limit:
+		print("%3d,%3d:" %(row,col))
+		# get the RGB values from the current pixel
+		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
+		if color_format == 'rgb':
+			print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
+		else:
+			print("\t", rgb2hex(r, g, b))
+		row -= codel
+		codel_count += 1
 
 # output for proof!
 print("")
 print("Width        = " + str(width) + " pixels")
 print("Height       = " + str(height) + " pixels")
 print("Codel size   = " + str(codel) + " pixels")
-print("Total Codels = " + str(pix) + ".")
-print("Total Codels = " + str(pix) + ".")
+print("Total Codels = " + str(codel_count) + ".")
