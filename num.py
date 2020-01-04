6a91557fa82ec722f4f2a75e15d8f237c5cd0ade
Fábián Gábor <info@fabiangabor.com>
2020-01-04 17:40:28 +0100

    print formats

--- num.py
+++ num.py
@@ -24,53 +24,68 @@ row = 0
 col = 0
 codel = 10
 codel_count = 0
-color_format = 'hex' # rgb / hex
+
+# Print color format:
+# HEX = 000
+# RGB = 111
+# R   = 100
+# G   = 010
+# B   = 001
+color_format = "100"
+
+# Print coordinates
+print_coords = 0
+
+def print_color_format(cf):
+	if cf == "000":
+		print("\t", rgb2hex(r, g, b))
+	if cf == "111":
+		print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
+	if cf == "100":
+		print("\t%3s" % (str(r)))
+	if cf == "010":
+		print("\t%3s" % (str(g)))
+	if cf == "001":
+		print("\t%3s" % (str(b)))
+
 
 # Get color from the middle of the codel:
 # col + codel / 2, row + codel / 2
 
 limit = 0
 while limit < size / 2:
-	print("limit = ", limit)
+	#print("limit = ", limit)
 	while col + codel < size - limit:
-		print("%3d,%3d:" %(row,col))
+		if print_coords:
+			print("%3d,%3d:" %(row,col))
 		#get the RGB values from the current pixel
 		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
-		if color_format == 'rgb':
-			print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
-		else:
-			print("\t", rgb2hex(r, g, b))
+		print_color_format(color_format)
 		col += codel
 		codel_count += 1
 	while row + codel < size - limit:
-		print("%3d,%3d:" %(row,col))
+		if print_coords:
+			print("%3d,%3d:" %(row,col))
 		# get the RGB values from the current pixel
 		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
-		if color_format == 'rgb':
-			print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
-		else:
-			print("\t", rgb2hex(r, g, b))
+		print_color_format(color_format)
 		row += codel
 		codel_count += 1
 	while col - codel >= limit:
-		print("%3d,%3d:" %(row,col))
+		if print_coords:
+			print("%3d,%3d:" %(row,col))
 		# get the RGB values from the current pixel
 		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
-		if color_format == 'rgb':
-			print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
-		else:
-			print("\t", rgb2hex(r, g, b))
+		print_color_format(color_format)
 		col -= codel
 		codel_count += 1
 	limit += codel * 2
 	while row - codel >= limit:
-		print("%3d,%3d:" %(row,col))
+		if print_coords:
+			print("%3d,%3d:" %(row,col))
 		# get the RGB values from the current pixel
 		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
-		if color_format == 'rgb':
-			print("\t(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
-		else:
-			print("\t", rgb2hex(r, g, b))
+		print_color_format(color_format)
 		row -= codel
 		codel_count += 1
 
