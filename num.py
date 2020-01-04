084d44b013411cf30793b385a9b3c2896b67af33
Fábián Gábor <info@fabiangabor.com>
2020-01-04 17:44:38 +0100

    python formatting

--- num.py
+++ num.py
@@ -6,12 +6,14 @@ from PIL import Image
 
 # Open our image
 im = Image.open("not_art.png")
- 
+
 # Convert our image to RGB
 rgb_im = im.convert('RGB')
 
+
 def rgb2hex(r, g, b):
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
 
 # Use the .size object to retrieve a tuple contain (width,height) of the image
 # and assign them to width and height variables
@@ -36,17 +38,18 @@ color_format = "111"
 # Print coordinates
 print_coords = 0
 
+
 def print_color_format(cf):
-	if cf == "000":
-		print(rgb2hex(r, g, b))
-	if cf == "111":
-		print("(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
-	if cf == "100":
-		print("%3s" % (str(r)))
-	if cf == "010":
-		print("%3s" % (str(g)))
-	if cf == "001":
-		print("%3s" % (str(b)))
+    if cf == "000":
+        print(rgb2hex(r, g, b))
+    if cf == "111":
+        print("(%3s,%3s,%3s)" % (str(r), str(g), str(b)))
+    if cf == "100":
+        print("%3s" % (str(r)))
+    if cf == "010":
+        print("%3s" % (str(g)))
+    if cf == "001":
+        print("%3s" % (str(b)))
 
 
 # Get color from the middle of the codel:
@@ -54,40 +57,40 @@ def print_color_format(cf):
 
 limit = 0
 while limit < size / 2:
-	#print("limit = ", limit)
-	while col + codel < size - limit:
-		if print_coords:
-			print("%3d,%3d:" %(row,col))
-		#get the RGB values from the current pixel
-		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
-		print_color_format(color_format)
-		col += codel
-		codel_count += 1
-	while row + codel < size - limit:
-		if print_coords:
-			print("%3d,%3d:" %(row,col))
-		# get the RGB values from the current pixel
-		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
-		print_color_format(color_format)
-		row += codel
-		codel_count += 1
-	while col - codel >= limit:
-		if print_coords:
-			print("%3d,%3d:" %(row,col))
-		# get the RGB values from the current pixel
-		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
-		print_color_format(color_format)
-		col -= codel
-		codel_count += 1
-	limit += codel * 2
-	while row - codel >= limit:
-		if print_coords:
-			print("%3d,%3d:" %(row,col))
-		# get the RGB values from the current pixel
-		r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
-		print_color_format(color_format)
-		row -= codel
-		codel_count += 1
+    # print("limit = ", limit)
+    while col + codel < size - limit:
+        if print_coords:
+            print("%3d,%3d:" % (row, col))
+        # get the RGB values from the current pixel
+        r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
+        print_color_format(color_format)
+        col += codel
+        codel_count += 1
+    while row + codel < size - limit:
+        if print_coords:
+            print("%3d,%3d:" % (row, col))
+        # get the RGB values from the current pixel
+        r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
+        print_color_format(color_format)
+        row += codel
+        codel_count += 1
+    while col - codel >= limit:
+        if print_coords:
+            print("%3d,%3d:" % (row, col))
+        # get the RGB values from the current pixel
+        r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
+        print_color_format(color_format)
+        col -= codel
+        codel_count += 1
+    limit += codel * 2
+    while row - codel >= limit:
+        if print_coords:
+            print("%3d,%3d:" % (row, col))
+        # get the RGB values from the current pixel
+        r, g, b = rgb_im.getpixel((col + codel / 2, row + codel / 2))
+        print_color_format(color_format)
+        row -= codel
+        codel_count += 1
 
 # output for proof!
 print("")
