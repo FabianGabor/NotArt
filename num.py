8f9c5ecab231b654a4a097d6e71fc917c3ad5f66
Fábián Gábor <info@fabiangabor.com>
2020-01-04 13:58:55 +0100

    .

--- num.py
+++ num.py
@@ -3,55 +3,42 @@
 #
 # Import the PIL library - pip3 install Pillow
 from PIL import Image
-<<<<<<< HEAD
 
 # Open our image
 im = Image.open("not_art_crop.png")
 
 # Convert our image to RGB
 rgb_im = im.convert('RGB')
-
-=======
  
 # Open our image
 im = Image.open("not_art_crop.png")
  
 # Convert our image to RGB
 rgb_im = im.convert('RGB')
- 
->>>>>>> master
+
 # Use the .size object to retrieve a tuple contain (width,height) of the image
 # and assign them to width and height variables
 width = rgb_im.size[0]
 height = rgb_im.size[1]
-<<<<<<< HEAD
 
-=======
- 
->>>>>>> master
 # set some counters for current row and column and total pixels
 row = 1
 col = 1
 pix = 0
 codel = 10
-<<<<<<< HEAD
 
 # create an empty output row
 rowdata = ""
 codels = ""
-
-=======
  
 # create an empty output row
 rowdata = ""
 codels = ""
- 
->>>>>>> master
+
 # loop through each pixel in each row outputting RGB value as we go...
 # all the plus and minus ones are to deal with the .getpixel class being
 # zero indexed and we want the output to start at pixel 1,1 not 0,0!
 while row < height + 1:
-<<<<<<< HEAD
     while col < width + 1:
         # get the RGB values from the current pixel
         r, g, b = rgb_im.getpixel((col - 1, row - 1))
@@ -74,7 +61,6 @@ while row < height + 1:
     # reset the column count
     col = 1
 
-=======
 	while col < width + 1:
 		# get the RGB values from the current pixel
 		r, g, b = rgb_im.getpixel((col - 1, row - 1))
@@ -96,8 +82,7 @@ while row < height + 1:
 	row = row + codel
 	# reset the column count
 	col = 1
- 
->>>>>>> master
+
 # output for proof!
 print("")
 print("Width        = " + str(width) + " pixels")
@@ -107,4 +92,3 @@ print("Codel size   = " + str(codel) + " pixels")
 print("Total Codels = " + str(pix) + ".")
 =======
 print("Total Codels = " + str(pix) + ".")
->>>>>>> master
